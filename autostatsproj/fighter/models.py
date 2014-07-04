f05c696d1facde4003173f565284fd15e928b539
aldnav <aldrinnavarro16@gmail.com>
2014-07-04 14:14:29 +0800

    Added initial model for Fighter

--- autostatsproj/fighter/models.py
+++ autostatsproj/fighter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Fighter(models.Model):
+	name = models.CharField(max_length=200)
+	age = models.IntegerField()
+	pub_date = models.DateTimeField('date published', default=timezone.now())
